c73b098a885ccb6015a0259411230a73eec32fe2
Kane Toomer <kanetoomer@yahoo.com>
2022-07-27 12:50:49 -0700

    finished cta for movies and shows.

--- src/components/HomeScreen/landing.js
+++ src/components/HomeScreen/landing.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 import '../../App.css';
 
+import poster1  from '../../assets/bevHillsCop.svg';
+import poster2  from '../../assets/goodfellas.svg';
+import poster3  from '../../assets/terminator.svg';
+import poster4  from '../../assets/pulpfiction.svg';
+import poster5 from '../../assets/friends.svg';
+import poster6  from '../../assets/heman.svg';
+import poster7 from '../../assets/inLivingColor.svg';
+import poster8  from '../../assets/cheers.svg';
+
 function Landing() {
 
     return (
         <>
             {/* Jumbotron */}
-            <section className="jumbotron p-4" id="header">
+            <section className="p-4" id="header">
                 {/* Navigation Bar */}
                 <nav className="navbar navbar-expand-lg">
                     <div className="container">
@@ -26,7 +35,7 @@ function Landing() {
                             <h1 className="display-5 fw-bold lh-1 mx-auto text-white mb-4">A Video Vault of 80's & 90's Classics</h1>
                             <p className="lead mb-5 text-white fw-normal">Someone said the best things in life are free... So, you can watch all of your favorites for free!</p>
                             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                                <button type="button" className="btn btn-primary btn-lg px-4">Get Started</button>
+                                <button type="button" className="btn btn-primary btn-lg py-3 px-5">Get Started</button>
                             </div>
                         </div>
                     </div>
@@ -34,77 +43,82 @@ function Landing() {
             </section>
 
             {/* MOVIES - Call to Action */}
-            <section className="jumbotron p-5" id="movie-jumbotron">
-                <div className="container p-5">
-                    <div className="container">
-                        <div className="row mb-5">
-                            <div className="col-lg-6 ">
-                                <h1 className="display-5 fw-bold lh-1 mx-auto text-white mb-4">The movies you love</h1>
-                                <p className="lead mb-5 text-white fw-normal">Fall in love with familiar favorites all over again in a growing collection right here on Zovy.</p>
-                                <button type="button" className="btn btn-primary btn-lg px-4">Get Started</button>
-                            </div>
-                        </div>
-                        <div className="row">
-
-
+            <div className="container col-xxl-8 px-4 py-5 my-5 rounded-4" id="movie-jumbotron">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                    <div className="col-10 col-sm-8 col-lg-6"></div>
+                    <div className="col-lg-6 pt-5">
+                        <h1 className="display-5 fw-bold lh-1 mb-3 text-white">The movies you love</h1>
+                        <p className="lead text-white">Fall in love with familiar favorites all over again in a growing collection right here on Zovy.</p>
+                        <div class="d-grid gap-3 d-md-flex justify-content-md-start">
+                            <button type="button" class="btn btn-primary btn-lg px-4 py-3 me-md-2 mt-3">Sign Up, it's free!</button>
                         </div>
                     </div>
                 </div>
-            </section>
+                <div className='row align-items-center g-5 pt-5 mt-5'>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster1} />
+                        </div>
+                    </div>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster2} />
+                        </div>
+                    </div>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster3} />
+                        </div>
+                    </div>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster4} />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             {/* SHOWS - Call to Action */}
-            <section className="jumbotron p-5" id="shows-jumbotron">
-                <div className="container p-5">
-                    <div className="container p-lg-5">
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <h1 className="display-6 fw-semibold mx-auto lh-1 text-white mb-4">Family time like it's 1995</h1>
-                                <p className="lead mb-5 text-white fw-normal">The best time is family time. What better way to spend it than to show the new generation the greatest the 80's and 90's had to offer!</p>
-                                <button type="button" className="btn btn-primary btn-lg px-4">Get Started</button>
-                            </div>
-                        </div>
-                        <div className="row">
-                            {/* MOVIE POSTERS GRID */}
+            <div className="container col-xxl-8 px-4 py-5 my-5 rounded-4" id="shows-jumbotron">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                    <div className="col-10 col-sm-8 col-lg-6"></div>
+                    <div className="col-lg-6 pt-5">
+                        <h1 className="display-5 fw-bold lh-1 mb-3 text-white">Family time like it's 1995</h1>
+                        <p className="lead text-white">The best time is family time. Watch all of your family favorites right here on Zovy.</p>
+                        <div class="d-grid gap-3 d-md-flex justify-content-md-start">
+                            <button type="button" class="btn btn-primary btn-lg px-4 py-3 me-md-2 mt-3">Sign Up, it's free!</button>
                         </div>
                     </div>
                 </div>
-            </section>
+                <div className='row align-items-center g-5 pt-5 mt-5'>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster5} />
+                        </div>
+                    </div>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster6} />
+                        </div>
+                    </div>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster7} />
+                        </div>
+                    </div>
+                    <div className='col-3 d-none d-md-block'>
+                        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                            <img src={poster8} />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             {/* WAYS TO WATCH - Call to Action */}
-            <section className="jumbotron" style={{ backgroundColor: 'purple' }}>
-                <div className="container p-5" style={{ backgroundColor: 'green' }}>
-                    <div className="container p-5" style={{ backgroundColor: 'orange' }}>
-                        <div className="row">
-                            <div className="col-lg-6 ">
-                                <h1 className="display-5 fw-bold lh-1 mx-auto text-white mb-4">Headline Text</h1>
-                                <p className="lead mb-5 text-white fw-normal">Subtext</p>
-                                <button type="button" className="btn btn-primary btn-lg px-4">Get Started</button>
-                            </div>
-                        </div>
-                        <div className="row">
-                            {/* MOVIE POSTERS GRID */}
-                        </div>
-                    </div>
-                </div>
-            </section>
+            
 
             {/*  LAST Call to Action */}
-            <section className="jumbotron" id="header">
-                <div className="container p-5">
-                    <div className="container p-5 glass-card">
-                        <div className="row">
-                            <div className="col-lg-6 mx-auto text-center">
-                                <h1 className="display-5 fw-bold lh-1 mx-auto text-white mb-4">A Video Vault of 80's & 90's Classics</h1>
-                                <p className="lead mb-5 text-white fw-normal">Stream all of your favorites for free!</p>
-                                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                                    <button type="button" className="btn btn-lg btn-primary">Get Started</button>
-                                </div>
-                                <a href="#" className="text-white fw-semibold">Already have an account? Sign In</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            
 
             {/* Footer */}
             <div class="container">
