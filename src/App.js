b8419cd8871fc937f3c367f71a862b1ac13a782c
Kane Toomer <kanetoomer@yahoo.com>
2022-07-29 11:44:26 -0700

    sign in and register ui complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Landing from './components/HomeScreen/landing';
+import Register from './components/Authentication/register';
+// import SignIn from './components/Authentication/signIn';
+// import Landing from './components/HomeScreen/landing';
 
 function App() {
   return (
-    <Landing />
+    <Register />
   );
 }
 
---
+++ src/components/Streaming/streaming.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import '../../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Streaming() {
+    return(
+        <>
+        </>
+    );
+}
+
+export default Streaming;
