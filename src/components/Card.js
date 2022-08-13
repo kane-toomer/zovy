4cd1fe524dacfe44a691b74f3510a05461101e0c
Kane Toomer <kanetoomer@yahoo.com>
2022-08-13 11:58:23 -0700

    frontend complete. figured out how to pass props to the modal without compromising the watch trailer modal using react hooks.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,45 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
+import { Modal } from 'react-bootstrap';
 
 function Card({ actors, genre, rated, released, plot, poster, title }) {
 
+    const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false);
+
+    // Modal 1
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // Modal 2
+    const handleClose2 = () => setShow2(false);
+    const handleShow2 = () => setShow2(true);
+
     return (
         <>
-            <a href="#movieModal" data-bs-toggle="modal" className='m-4' style={{ height: 300, width: 200 }}>
-                <img src={poster} style={{ height: 300, width: 200 }} className="card card-cover overflow-hidden text-bg-dark rounded-2 shadow-lg" />
+            <a onClick={handleShow} className='m-4' style={{ height: 300, width: 200 }}>
+                <img src={poster} style={{ height: 300, width: 200 }} alt="Poster of movie/show from api" className="card card-cover overflow-hidden text-bg-dark rounded-2 shadow-lg" />
             </a>
 
-            {/* <!-- Modal --> */}
-            <div className="modal modal-sheet fade" id="movieModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header pb-4 border-bottom-0">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="d-flex">
-                                <div className="card card-cover overflow-hidden text-bg-dark rounded-2 shadow-lg">
-                                    <img src={poster} style={{ height: 225, width: 150 }} />
-                                </div>
-                                <div>
-                                    <h2 className="modal-title fw-bold ms-3 mb-3" id="exampleModalLabel">{title}</h2>
-                                    <p className="ms-3"><strong>Release Date:</strong> {released}</p>
-                                    <p className="ms-3"><strong>Genre:</strong> {genre}</p>
-                                    <p className="ms-3"><strong>Rating:</strong> {rated}</p>
-                                </div>
+            {/* Modal #1 */}
+            <Modal show={show} animation={true} className="modal modal-sheet fade" id="movieModal" tabindex="-1" aria-labelledby="movieModal" aria-hidden="true">
+                <div className="modal-content">
+                    <div className="modal-header pb-4 border-bottom-0">
+                        <button type="button" class="btn-close" onClick={handleClose}></button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="d-flex">
+                            <div className="card card-cover overflow-hidden text-bg-dark rounded-2 shadow-lg">
+                                <img src={poster} style={{ height: 225, width: 150 }} alt="Poster of movie/show from api" />
                             </div>
-                            <div className="container mt-3">
-                                <p className="pb-2"><strong>Staring:</strong> {actors}</p>
-                                <p className="pb-2"><strong>Synopsis: </strong> {plot}</p>
+                            <div>
+                                <h2 className="modal-title fw-bold ms-3 mb-3" id="exampleModalLabel">{title}</h2>
+                                <p className="ms-3"><strong>Release Date:</strong> {released}</p>
+                                <p className="ms-3"><strong>Genre:</strong> {genre}</p>
+                                <p className="ms-3"><strong>Rating:</strong> {rated}</p>
                             </div>
                         </div>
-                        <div className="modal-footer border-top-0">
-                            <button type="button" className="w-100 btn btn-primary">Watch Trailer</button>
+                        <div className="container mt-3">
+                            <p className="pb-2"><strong>Staring:</strong> {actors}</p>
+                            <p className="pb-2"><strong>Synopsis: </strong> {plot}</p>
+                        </div>
+                    </div>
+                    <div className="modal-footer border-top-0">
+                        <a onClick={handleShow2} data-bs-toggle="modal" type="button" className="w-100 btn btn-primary">Watch Trailer</a>
+                    </div>
+                </div>
+            </Modal>
+
+            {/* Modal #2 */}
+            <Modal show={show2} animation={true} className="modal modal-sheet fade" id="trailerModal" tabindex="-1" aria-labelledby="trailerModal" aria-hidden="true">
+                <div className="modal-content">
+                    <div className="modal-header border-bottom-0">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" onClick={handleClose2}></button>
+                    </div>
+                    <div className="modal-body pb-4 mb-2">
+                        <p className="lead">I have watched this interview a few times and I feel it can be credited to the creation of a lot of start ups and thousands of jobs created as a result. Enjoy.</p>
+                        <div className="d-flex justify-content-center">
+                            <iframe
+                                title="trailer"
+                                id="trailer"
+                                width="560"
+                                height="315"
+                                src="https://www.youtube.com/embed/Lb4IcGF5iTQ"
+                                frameborder="0"
+                                allowfullscreen
+                            >
+                            </iframe>
                         </div>
                     </div>
                 </div>
-            </div>
-
+            </Modal>
         </>
     );
 }
