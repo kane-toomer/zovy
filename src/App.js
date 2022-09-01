13777a6373fbc4dbac015a1ce5d7e7acd7cdaf8e
Kane Toomer <kanetoomer@yahoo.com>
2022-08-31 19:24:38 -0700

    git push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Landing from './components/Landing';
 import SignIn from './components/SignIn';
@@ -9,14 +9,12 @@ import Register from './components/Register';
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/browse" element={<Streaming />} />
-      </Routes>
-    </Router >
+    <Routes>
+      <Route exact path="/" element={<Landing />} />
+      <Route exact path="/signin" element={<SignIn />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/browse" element={<Streaming />} />
+    </Routes>
   );
 
 }
